d291fea394920e5d599b62674ef3bb8ef115d467
AdilTehranli <adiljt@code.edu.az>
2023-06-14 00:20:24 +0400

    Updated

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+"use strict "
+
+
+let icon=document.querySelector('.icon');
+icon.addEventListener('click',()=>{
+    let mobile=document.querySelector('.mobile')
+    mobile.classList.toggle('active')
+})
+
